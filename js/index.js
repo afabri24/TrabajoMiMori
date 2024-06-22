8688811becd2322d40ee2985761e61a32a4c953e
Angel Fabrizio Franyutti Pulido <afabri24@gmail.com>
2024-06-21 22:45:13 -0600

    primer push

---
+++ js/index.js
@@ -0,0 +1,43 @@
+//Ejercicio 1
+let color="rojo";
+console.log(color);
+color="verde";
+console.log(color)
+//Ejercicio 2
+let num1=12
+let num2=13
+console.log(num1 +num2);
+//Ejercicio 3
+const amor="YO AMO A MORIEL";
+//amor="amo a mi pc";
+console.log(amor);
+//Ejercicio 4
+const radio=100;
+console.log(radio*2);
+//Ejercicio 5
+let numeros=[1,2,3,4,5,6,7,8,9,10];
+numeros.push(11);
+console.log(numeros);
+//Ejercicio 6
+let legumbres=["arroz","lentejas","habas","frijoles"];
+legumbres.pop();
+console.log(legumbres);
+//Ejercicio 7
+let persona={
+    nombre:"Moriel",
+    edad:22
+}
+console.log(persona.nombre);
+console.log(persona.edad);
+//Ejercicio 8
+let coche={
+    marca:"Kia",
+    modelo:"gol"
+}
+coche.modelo="Superior";
+console.log(coche.modelo);
+
+function SacarArea(a,b){
+    let area=a*b;
+    console.log(area);
+}
